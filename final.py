82a21012cbcc6e1ce0dc3a15ff611de372c42926
luckeyme74 <jmurphy1364@mchenry.edu>
2016-12-08 19:42:52 -0600

    Add files via upload

--- final.py
+++ final.py
@@ -1,27 +1,33 @@
-print (">*< >*< >*< >*< >*< >*< >*<")
-print (" Please choose an option:\n")
-print ("1. Returning Customer")
-print ("2. New Customer")
-print ("3. Guest")
-print (">*< >*< >*< >*< >*< >*< >*<")
-entry = int(raw_input("Enter option:"))
-while entry < 1:
-    print ("Please choose option 1, 2 or 3.\n")
+def returning():
+    if entry == 1:
+        print ("Returning Customer.")
+
+def guest():
+    if entry == 3:
+        print ("Welcome! We hope to see you often!")
+
+def new():
+    if entry == 2:
+        print ("New Customer")
+
+entry = 0
+while entry >=0:
     print (">*< >*< >*< >*< >*< >*< >*<")
-    print (" Please choose an option:")
+    print (" Please choose an option:\n")
     print ("1. Returning Customer")
     print ("2. New Customer")
     print ("3. Guest")
     print (">*< >*< >*< >*< >*< >*< >*<")
-    entry = int(raw_input("Enter option:"))
-while entry > 3:
-    print ("Please choose option 1, 2 or 3.\n")
-    print (">*< >*< >*< >*< >*< >*< >*<")
-    print (" Please choose an option:")
-    print ("1. Returning Customer")
-    print ("2. New Customer")
-    print ("3. Guest")
-    print (">*< >*< >*< >*< >*< >*< >*<")
-    entry = int(raw_input("Enter option:"))
-if entry == 3:
-    print ("Welcome! We hope to see you often!")
+    entry = int(raw_input("Enter option:     "))
+    if entry < 1 or entry > 3:
+        entry = int(raw_input("Please enter Customer option 1, 2 or 3:     "))
+    elif entry == 1:
+        returning()
+        entry = -1
+    elif entry == 2:
+        new()
+        entry = -1
+    elif entry == 3:
+        guest()
+        entry = -1
+
